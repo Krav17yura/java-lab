db95b1076fbc462a9ca5e301500bb7479af29c2e
Yura <krav17yura@gmail.com>
2020-05-13 20:27:11 +0200

    third commit

--- laba-14/src/com/company/Main.java
+++ laba-14/src/com/company/Main.java
@@ -1,6 +1,4 @@
 package com.company;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,72 +6,86 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        System.out.print("Enter Arr length: ");
+        Scanner sc = new Scanner(System.in);
+        int arrLength = sc.nextInt();
+
+        int[] arr = new int[arrLength];
+        Random rn = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rn.nextInt(100);
+        }
+
+        long time = System.nanoTime();
         int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Однопоток:" + sum);
+        time = System.nanoTime() - time;
+        System.out.printf("Elapsed %,9.3f ms\n", time / 1_000_000.0);
+
+
+        MultyThreadSum  Sum = new MultyThreadSum ();
+        sum = Sum.Sum(arr);
+        long time2 = System.nanoTime();
+        System.out.println("Многопоток:" + sum);
+        time2 = System.nanoTime() - time2;
+        System.out.printf("Elapsed %,9.3f ms\n", time2 / 1_000_000.0);
+    }
+
+    static class MultyThreadSum  {
         int numberOfThreads = 10;
 
-          System.out.print("Enter Arr length: ");
-          Scanner sc = new Scanner(System.in);
-          int arrLength = sc.nextInt();
+        public int Sum(int arr[]) throws InterruptedException {
+            int midleIndex  = arr.length / numberOfThreads;
 
-          int[] arr = new int[arrLength];
-          Random rn = new Random();
-          for(int  i = 0; i < arr.length; i++){
-              arr[i] = rn.nextInt(100);
-          }
-        long time = System.nanoTime();
+            SumThreadnNumber[] Thread = new SumThreadnNumber[numberOfThreads];
 
-          for(int i = 0; i < arr.length; i++){
-           sum += arr[i];
-          }
-          System.out.println(sum);
+            for (int i = 0; i < numberOfThreads; i++) {
+                int beginIndex  = i * midleIndex ;
+                int endIndex = (i + 1) * midleIndex ;
+                if (endIndex > arr.length) {
+                    endIndex = arr.length;
+                }
+                Thread[i] = new SumThreadnNumber(beginIndex , endIndex, arr);
+                Thread[i].start();
+            }
 
-        time = System.nanoTime() - time;
-        System.out.printf("Elapsed %,9.3f ms\n", time/1_000_000.0);
-
-
-        long time2 = System.nanoTime();
-        sum = 0;
-        int beginIndex = 0;
-        int midleIndex = arrLength/numberOfThreads;
-        for(int i = 0; i < numberOfThreads; i++){
-            int[] threadArr = Arrays.copyOfRange(arr, beginIndex, midleIndex);
-            beginIndex = midleIndex;
-            midleIndex += arrLength/numberOfThreads;
-            MultyThreadSum thr = new MultyThreadSum(threadArr, arrLength/numberOfThreads, "Thread #" + i);
-            thr.start();
-            thr.join();
-            sum += thr.Res();
-            System.out.println("Result thread - " + i + ": " + thr.Res());
+            int sum = 0;
+            for (int i = 0; i < numberOfThreads; i++) {
+                Thread[i].join();
+                sum += Thread[i].Res();
+            }
+            return sum;
         }
-        System.out.println("Result: " + sum);
-        time2 = System.nanoTime() - time2;
-        System.out.printf("Elapsed %,9.3f ms\n", time2/1_000_000.0);
-
-
-
     }
 
-    public static class MultyThreadSum extends Thread
-    {
-        int[] arr;
-        int numberOfTherad;
-        int result = 0;
+    static class SumThreadnNumber extends Thread {
+        int beginIndex;
+        int endIndex;
+        int[] arrr;
+        int result;
 
-        public MultyThreadSum(int[] arr, int numberOfTherad, String name)
-        {
-            super(name);
-            this.numberOfTherad = numberOfTherad;
-            this.arr = new int[this.numberOfTherad];
-            this.arr = arr;
+        public SumThreadnNumber(int first, int last, int[] arr) {
+            this.beginIndex = first;
+            this.endIndex = last;
+            arrr = arr;
         }
 
+        @Override
         public void run() {
-            for (int i = 0; i < arr.length; i++)
-                result += arr[i];
+            for (int i = beginIndex; i < endIndex; i++) {
+                result += arrr[i];
+            }
         }
-
         public int Res()
         {return result;}
-
     }
 }
+
+
+
+
+
